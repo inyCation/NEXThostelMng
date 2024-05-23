eda11267fa599ce300c7c9ce2a4701c1b3b07d6e
Deepak Raj <3razz@gmail.com>
2024-05-23 21:52:01 +0530

    951

--- app/api/featuredHostels/route.ts
+++ app/api/featuredHostels/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextResponse){
+export async function GET(req:NextRequest){
     return NextResponse.json({
         test:"THIS NEXT.js"
     })
