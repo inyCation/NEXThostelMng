f0e0ea7c9e21430994163c84a39a85fdc7afd129
Deepak Raj <3razz@gmail.com>
2024-06-15 08:51:10 +0530

    15062024085108

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req:NextRequest) {
     if(req.method ==="GET"){
-        
         try {
             const response = NextResponse.json(
                 {
